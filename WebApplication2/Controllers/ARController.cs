740d5b618377f75b3ac7cfb1afcb526dc75a55e5
黃大都 <chingchou@gmail.com>
2016-03-12 13:46:39 +0800

    26 練習不同的 ActionResult 用法 ( View, PartialView, Content, File )

---
+++ WebApplication2/Controllers/ARController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class ARController : Controller
+    {
+        // GET: AR
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return PartialView("Index");
+        }
+
+        public ActionResult ContectTest()
+        {
+            return Content("<script>alert('Redriecting ...');</script>",
+                "application/javascript", Encoding.UTF8);
+        }
+
+        public ActionResult FileTest()
+        {
+            return File(Server.MapPath("~/Content/alphago-logo.png"), "image/png");
+
+            //return File(Server.MapPath("~/Content/alphago-logo.png"), "image/png","bigdoo.png");
+        }
+    }
+}
--- WebApplication2/Global.asax.cs
+++ WebApplication2/Global.asax.cs
@@ -18,6 +18,9 @@ namespace WebApplication2
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+
         }
     }
 }
