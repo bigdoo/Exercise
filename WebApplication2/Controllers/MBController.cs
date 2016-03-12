d92026279394b4f563fb13b4228b3c480d247a62
黃大都 <chingchou@gmail.com>
2016-03-12 15:40:12 +0800

    29 練習簡單模型繫結 (Simple Model Binding) 的兩種用法

---
+++ WebApplication2/Controllers/MBController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class MBController : Controller
+    {
+        // GET: MB
+        public ActionResult Index()
+        {
+            return View();
+        }
+         [HttpPost]
+         public ActionResult Index(String Name,DateTime Birthday)
+         {
+             return Content(Name + "" + Birthday);
+         }
+
+        /* [HttpPost]
+         public ActionResult Index(FormCollection form)
+         {
+             return Content(form["Name"] + "" + form["Birthday"]);
+         }*/
+
+         /*[HttpPost]//不建議舊寫法
+         public ActionResult Index(int a)
+         {
+            return Content(Request.Form["Name"] + " " + Request.Form["Birthday"]);
+        }*/
+    }
+}
