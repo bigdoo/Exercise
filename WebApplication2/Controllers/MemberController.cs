4da6e02e4e8f017fbad499f1259c3f31557dc31a
黃大都 <chingchou@gmail.com>
2016-03-13 11:16:37 +0800

    34 練習透過表單驗證 (FormsAuthentication) 實作登入、登出機制

---
+++ WebApplication2/Controllers/MemberController.cs
@@ -0,0 +1,45 @@
+﻿using WebApplication2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace WebApplication2.Controllers
+{
+    [Authorize]
+    public class MemberController : Controller
+    {
+        // GET: Member
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(LoginViewModel login)
+        {
+            if (checkedLogin(login.Email,login.Password))
+            {
+                FormsAuthentication.RedirectFromLoginPage(login.Email, false);
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        private bool checkedLogin(string username, string password)
+        {
+             return (
+                username == "doggy.huang@gmail.com" &&
+                password == "123"
+             );
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
