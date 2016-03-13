c4b9f4faa536a2c8a0ae3dbf1934644d12c18a9a
黃大都 <chingchou@gmail.com>
2016-03-13 11:38:11 +0800

    35 練習透過 HandlerError 自訂不同例外狀況下的自訂錯誤畫面

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -28,6 +29,21 @@ namespace WebApplication2.Controllers
         public ActionResult Test()
         {
             return View();
+        }
+        [HandleError(ExceptionType = typeof(ArgumentException), View = "ErrorArgument")]
+        [HandleError(ExceptionType = typeof(SqlException), View = "ErrorSql")]
+        public ActionResult ErrorTest(String e)
+        {
+            if (e == "1")
+            {
+                throw new ArgumentException("Error 1");
+            }
+
+            if (e == "2")
+            {
+                throw new Exception("Error 2");
+            }
+            return Content("No Error");
         }
     }
 }
